30ebc2799163af622b479e6bf0e7f3281d386bcb
Crystel Montero <crysvane05@estudiantec.cr>
2021-10-29 21:49:47 -0600

    Nombres

--- src/Vista/Main.java
+++ src/Vista/Main.java
@@ -1,3 +1,11 @@
+/**
+ * Fabrizio Ferreto 2019177147
+ * Ulises Rodríguez 2019380067
+ * Natalia Vargas   2019077180
+ * Fernando Álvarez 2019171657
+ * Crystel Montero  2019158736
+* */
+
 package Vista;
 
 import javafx.application.Application;
